f65f8bb8d386b65a8cc9f9276951243b46e8f03b
lucasgdiez <lucas@bytery.com>
2019-10-22 01:17:41 -0300

    destructuring applied!

---
+++ part1/src/components/Greeting.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Hello = ({name, age}) => {
+    const bornYear = () => {
+        const yearNow = new Date().getFullYear();
+        return yearNow - age;
+    }
+
+    return (
+        <div>
+            <p> Hello {name}, you are {age} old. </p>
+            <p>So you were probably born in {bornYear()}</p>
+        </div>
+    )
+}
+
+export default Hello;
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,75 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from './components/Header';
-import Content from './components/Content';
-import Total from './components/Total';
+import Hello from './components/Greeting';
 
 
 const App = () => {
-    // const course = 'Half Stack application development';
-    // const part1 = 'Fundamentals of React';
-    // const exercises1 = 10;
-    // const part2 = 'Using props to pass data';
-    // const exercises2 = 7;
-    // const part3 = 'State of a component';
-    // const exercises3 = 14;
-
-    // const part1 = {
-    //     name: 'Fundamentals of React',
-    //     exercises: 10
-    // };
-
-    // const part2 = {
-    //     name: 'Using props to pass data',
-    //     exercises: 7
-    // };
-
-    // const part3 = {
-    //     name: 'State of a component',
-    //     exercises: 14
-    // };
-
-    // const parts = [
-    //     {
-    //         name: 'Fundamentals of React',
-    //         exercises: 10
-    //     }, 
-    //     {
-    //         name: 'Using props to pass data',
-    //         exercises: 7
-    //     }, 
-    //     {
-    //         name: 'State of a component',
-    //         exercises: 14
-    //     }
-    // ];
-    // const exercises = [parts[0].exercises, parts[1].exercises, parts[2].exercises]
-
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            }, 
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            }, 
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
-
+    const name = 'Peter';
+    const age = '10'
     return(
         <>
-            <Header course={course.name}/>
-            <Content textContent={course.parts} />
-            <Total exercises={[course.parts[0].exercises, course.parts[1].exercises, course.parts[2].exercises]}/>
-            <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
+            <h1>Greetings</h1>
+            <Hello name="Maya" age={26 + 10} />
+            <Hello name={name} age={age} />
         </>
     )
 }
