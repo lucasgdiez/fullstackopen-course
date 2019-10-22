fbb2bb3cb1e8a28d51ab969da6df5c319aa2005b
lucasgdiez <lucas@bytery.com>
2019-10-22 00:23:35 -0300

    1.3 done

--- part1/src/index.js
+++ part1/src/index.js
@@ -8,31 +8,37 @@ import Total from './components/Total';
 
 const App = () => {
     const course = 'Half Stack application development';
-    const part1 = 'Fundamentals of React';
-    const exercises1 = 10;
-    const part2 = 'Using props to pass data';
-    const exercises2 = 7;
-    const part3 = 'State of a component';
-    const exercises3 = 14;
+    // const part1 = 'Fundamentals of React';
+    // const exercises1 = 10;
+    // const part2 = 'Using props to pass data';
+    // const exercises2 = 7;
+    // const part3 = 'State of a component';
+    // const exercises3 = 14;
 
-    const parts = [
-        {part1, exercises1},
-        {part2, exercises2},
-        {part3, exercises3},
-    ]
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    };
 
-    const exercises = [
-        exercises1,
-        exercises2,
-        exercises3
-    ]
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    };
+
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    };
+
+    const parts = [part1, part2, part3];
+    const exercises = [part1.exercises, part2.exercises, part3.exercises]
 
     return(
         <>
             <Header course={course}/>
             <Content textContent={parts} />
             <Total exercises={exercises}/>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
         </>
     )
 }
