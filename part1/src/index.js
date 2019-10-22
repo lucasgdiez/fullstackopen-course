7c39f84958f553c8cbe4ae8c561e18a3067f41a3
lucasgdiez <lucas@bytery.com>
2019-10-22 01:31:22 -0300

    added factory for value setting

--- part1/src/index.js
+++ part1/src/index.js
@@ -5,12 +5,26 @@ import ReactDOM from 'react-dom';
 const App = (props) => {
     const [ counter, setCounter ] = useState(0);
 
-    const timeout = setTimeout(() => setCounter(counter + 1), 1000);
+    const handleSum = () => {
+        setCounter(counter + 1);
+    }
+
+    const handleSbstrc = () => {
+        setCounter(counter - 1);
+    }
+
+    const handleReset = () => {
+        setCounter(0)
+    }
+
+    const setToValue = (value) => setCounter(value);
 
     return(
         <>
             <h1>Greetings {counter}</h1>
-            <button onClick={() => clearTimeout(timeout)}>Stop Counter</button>
+            <button onClick={() => setToValue(counter + 1)}>+</button>
+            <button onClick={() => setToValue(counter - 1)}>-</button>
+            <button onClick={() => setToValue(0)}>RESET</button>
         </>
     )
 }
