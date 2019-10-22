0e245fad23f1f3604a1c99252c09ba3eeb019ab3
lucasgdiez <lucas@bytery.com>
2019-10-22 01:24:12 -0300

    added lil bonus!

--- part1/src/index.js
+++ part1/src/index.js
@@ -5,11 +5,12 @@ import ReactDOM from 'react-dom';
 const App = (props) => {
     const [ counter, setCounter ] = useState(0);
 
-    setTimeout(() => setCounter(counter + 1), 1000);
+    const timeout = setTimeout(() => setCounter(counter + 1), 1000);
 
     return(
         <>
             <h1>Greetings {counter}</h1>
+            <button onClick={() => clearTimeout(timeout)}>Stop Counter</button>
         </>
     )
 }
