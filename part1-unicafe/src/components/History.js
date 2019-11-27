398a2c4ea6bdfaeef3380fce710f7a96840f937d
lucasgdiez <lucasgdiez@gmail.com>
2019-11-27 10:31:11 -0300

    Update History.js

--- part1-unicafe/src/components/History.js
+++ part1-unicafe/src/components/History.js
@@ -1,5 +1,13 @@
 import React from "react";
 
+const totalAverage = (...props) => {
+  const reducedValues = props.reduce((prevVal, currentValue) => {
+    return (prevVal + currentValue) / props.length;
+  }, 0);
+
+  return reducedValues.toFixed(2);
+};
+
 const History = ({ allValues }) => {
   return (
     <div>
@@ -12,6 +20,24 @@ const History = ({ allValues }) => {
         </li>
         <li style={{ listStyle: "none" }}>
           Bad: {allValues.Bad > 0 ? <span>{allValues.Bad}</span> : "Empty."}
+        </li>
+      </ul>
+      <ul style={{ padding: "0" }}>
+        <li style={{ listStyle: "none", fontWeight: 600 }}>
+          All:{" "}
+          {allValues.Good || allValues.Neutral || allValues.Bad > 0 ? (
+            <span>{allValues.Good + allValues.Neutral + allValues.Bad}</span>
+          ) : (
+            "Empty."
+          )}
+        </li>
+        <li style={{ listStyle: "none", fontWeight: 600 }}>
+          Average:{" "}
+          {allValues.Good || allValues.Neutral || allValues.Bad > 0 ? (
+            <span>{totalAverage(allValues.Good, allValues.Neutral, allValues.Bad)}</span>
+          ) : (
+            "Empty."
+          )}
         </li>
       </ul>
     </div>
