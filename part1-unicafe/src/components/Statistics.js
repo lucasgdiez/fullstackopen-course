f12320c15631acb3d935f50741994cc01c5190cb
lucasgdiez <lucasgdiez@gmail.com>
2019-11-28 16:37:38 -0300

    Step 4 completed!

--- part1-unicafe/src/components/Statistics.js
+++ part1-unicafe/src/components/Statistics.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import History from "./History";
 
 const Statistics = ({ allValues }) => {
+  const values = Object.values(allValues).map((val) => {
+    return val;
+  });
+
   return (
     <div>
       <h3>Statistics</h3>
-      <History {...allValues} />
+      {values.reduce((prevVal, currentVal) => {
+        return prevVal + currentVal;
+      }) === 0 ? (
+        "No Feedback Given."
+      ) : (
+        <History {...allValues} />
+      )}
     </div>
   );
 };
